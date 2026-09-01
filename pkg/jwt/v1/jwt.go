82809cff5cbd50384acbd13311f2faa927f186fd
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-09-01 10:50:30 +0200

    attempted to make a JWT manager

--- pkg/jwt/v1/jwt.go
+++ pkg/jwt/v1/jwt.go
@@ -4,52 +4,32 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"slices"
-	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 	httphelper "github.com/kjetils-labs/go-utils/pkg/http/v1"
 )
 
-type Scope string
+// Ensure that JWTManager implements the Manager interface
+var _ Manager = (*JWTManager)(nil)
 
-// We use iota to define the scope levels, where ScopeAdmin has the highest level of access, followed by ScopeWrite and ScopeRead.
-// It lets us easily compare the scope level is equal or lower than the required scope level for a given operation.
-const (
-	ScopeAdmin = iota
-	ScopeWrite
-	ScopeRead
-)
+type Manager interface {
+	CreateToken(Ctx context.Context, claims jwt.Claims) (string, error)
 
-func ParseScope(scope int) Scope {
-	switch scope {
-	case ScopeAdmin:
-		return "admin"
-	case ScopeWrite:
-		return "write"
-	case ScopeRead:
-		return "read"
-	default:
-		return "unknown"
-	}
+	ParseAndValidateToken(Ctx context.Context, tokenString string, claims jwt.Claims) (*jwt.Token, error)
 }
 
-var secretKey = []byte("b7351121-4d41-4767-beae-a3dab4c6f275")
+type JWTManager struct {
+	secretKey []byte
+}
 
-type User struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+func NewJWTManager(secretKey []byte) Manager {
+	return &JWTManager{
+		secretKey: secretKey,
+	}
 }
 
 type LoginResponse struct {
 	Token string `json:"token"`
-}
-
-type Claims struct {
-	Username string `json:"username"`
-	Exp      int64  `json:"exp"`
-	Scope    int    `json:"scope"`
-	jwt.RegisteredClaims
 }
 
 func NewLoginResponse(token string) *LoginResponse {
@@ -58,16 +38,12 @@ func NewLoginResponse(token string) *LoginResponse {
 	}
 }
 
-func CreateToken(ctx context.Context, username string, scope int) (string, error) {
+func (j *JWTManager) CreateToken(ctx context.Context, claims jwt.Claims) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
-		Claims{
-			Username: username,
-			Exp:      time.Now().Add(time.Hour * 24).Unix(),
-			Scope:    scope,
-		})
+		claims)
 
-	tokenString, err := token.SignedString(secretKey)
+	tokenString, err := token.SignedString(j.secretKey)
 	if err != nil {
 		return "", err
 	}
@@ -75,13 +51,13 @@ func CreateToken(ctx context.Context, username string, scope int) (string, error
 	return tokenString, nil
 }
 
-func parseAndVerifyToken(_ context.Context, tokenString string, claims *Claims) (*jwt.Token, error) {
+func (j *JWTManager) ParseAndValidateToken(_ context.Context, tokenString string, claims jwt.Claims) (*jwt.Token, error) {
 
 	keyFunc := func(token *jwt.Token) (any, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
-		return secretKey, nil
+		return j.secretKey, nil
 	}
 
 	token, err := jwt.ParseWithClaims(tokenString, claims, keyFunc)
@@ -96,40 +72,7 @@ func parseAndVerifyToken(_ context.Context, tokenString string, claims *Claims) 
 	return token, nil
 }
 
-func VerifyToken(tokenString string) error {
-
-	jwtToken, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
-		return secretKey, nil
-	})
-
-	if err != nil {
-		return err
-	}
-
-	if !jwtToken.Valid {
-		return fmt.Errorf("invalid token")
-	}
-
-	return nil
-}
-
-func ParseToken(ctx context.Context, tokenString string) (*Claims, error) {
-
-	claims := &Claims{}
-	_, err := parseAndVerifyToken(ctx, tokenString, claims)
-	if err != nil {
-		return nil, err
-	}
-
-	return claims, nil
-}
-
-func ValidateScope(scope string, allowedScopes ...Scope) bool {
-
-	return false
-}
-
-func AuthenticationMiddleware(next http.Handler) http.Handler {
+func AuthenticationMiddleware(next http.Handler, secret string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		tokenString := r.Header.Get("Authorization")
 		if tokenString == "" {
@@ -137,16 +80,10 @@ func AuthenticationMiddleware(next http.Handler) http.Handler {
 			return
 		}
 
-		err := VerifyToken(tokenString)
-		if err != nil {
-			httphelper.WriteJSONError(w, "Invalid token", http.StatusUnauthorized)
-			return
-		}
+		manager := NewJWTManager([]byte(secret))
 
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
-			return secretKey, nil
-		})
-
+		claims := &jwt.MapClaims{}
+		token, err := manager.ParseAndValidateToken(r.Context(), tokenString, claims)
 		if err != nil {
 			httphelper.WriteJSONError(w, "Invalid token", http.StatusUnauthorized)
 			return
@@ -158,14 +95,5 @@ func AuthenticationMiddleware(next http.Handler) http.Handler {
 		}
 
 		next.ServeHTTP(w, r)
-	})
-}
-
-func AuthorizeScope(scope int, allowedScopes ...int) bool {
-	return slices.ContainsFunc(allowedScopes, func(s int) bool {
-		if s <= scope {
-			return true
-		}
-		return false
 	})
 }
